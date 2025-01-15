b7577e87f3a807712b66c99a3667dfa47c07fa88
Dmytro Chobotar <dchobotar.fl@gmail.com>
2025-01-15 16:15:12 -0500

    initial commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './components/App.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
