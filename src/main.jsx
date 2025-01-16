f3d0921a86c0cb854f68eea5c27d3877e82daf3e
Dmytro Chobotar <dchobotar.fl@gmail.com>
2025-01-16 17:35:08 -0500

    Imrovements in Readme, index,added animate.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import './index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './components/App.jsx';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
